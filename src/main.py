199b465c79044b0b8c2240dfc4861e79f6f2891a
Junior Mayhe <juniormayhe@gmail.com>
2024-11-01 16:59:04 +0000

    handling UI changes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-from selenium.common.exceptions import WebDriverException, NoSuchDriverException
+from selenium.common.exceptions import WebDriverException, NoSuchDriverException, TimeoutException
 
 import time
 import requests
@@ -103,73 +106,85 @@ except (WebDriverException, NoSuchDriverException) as e:
         exit()
 
 #driver.get('chrome-extension://'+extensionId+'/index.html')
-print('Started! Logging in...')
+
 driver.get('https://app.getgrass.io/')
+print('Started! Accepting cookies...')
+# Add this block right before trying to access the user input fields
+try:
+    # Wait for the "ACCEPT ALL" button to be clickable
+    accept_button = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "ACCEPT")]'))
+    )
+    accept_button.click()
+    print("Clicked 'ACCEPT ALL' button.")
+except TimeoutException:
+    print("Accept button not clickable or not found.")
+    driver.quit()
+    exit()
 
-sleep = 0
-while True:
-    try:
-        driver.find_element('xpath', '//*[@name="user"]')
-        driver.find_element('xpath', '//*[@name="password"]')
-        driver.find_element('xpath', '//*[@type="submit"]')
-        break
-    except:
-        time.sleep(1)
-        print('Loading login form...')
-        sleep += 1
-        if sleep > 15:
-            print('Could not load login form! Exiting...')
-            generate_error_report(driver)
-            driver.quit()
-            exit()
+print('Logging in...')
+try:
+    
+    user =WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@name="user"]'))
+    )
+    time.sleep(1)
+    user.send_keys(USER)
+except TimeoutException:
+    print("Username input not found.")
+    driver.quit()
+    exit()
 
-#find name="user"
-user = driver.find_element('xpath', '//*[@name="user"]')
-passw = driver.find_element('xpath', '//*[@name="password"]')
-submit = driver.find_element('xpath', '//*[@type="submit"]')
+try:
+    passw = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@name="password"]'))
+    )
+    time.sleep(1)
+    passw.send_keys(PASSW)
+except TimeoutException:
+    print("Password input not found.")
+    driver.quit()
+    exit()
 
-#get user from env
-user.send_keys(USER)
-passw.send_keys(PASSW)
-submit.click()
+# Wait for the submit button to be clickable and enabled, then click it
+try:
+    submit = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@type="submit"]'))
+    )
+    time.sleep(1)
+    submit.click()
+except TimeoutException:
+    print("Submit button not clickable.")
+    driver.quit()
+    exit()
 
-#id="chakra-toast-manager-top-right" is the toast
-
-
-sleep = 0
-while True:
-    try:
-        e = driver.find_element('xpath', '//*[contains(text(), "Dashboard")]')
-        break
-    except:
-        time.sleep(1)
-        print('Logging in...')
-        sleep += 1
-        if sleep > 30:
-            print('Could not login! Double Check your username and password! Exiting...')
-            generate_error_report(driver)
-            driver.quit()
-            exit()
+try:
+    # Wait for the "Dashboard" heading to appear
+    dashboard_heading = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '//h2[contains(text(), "Dashboard")]'))
+    )
+    print("Dashboard loaded successfully!")
+except TimeoutException:
+    print("Dashboard did not load. Exiting.")
+    driver.quit()
+    exit()
 
 print('Logged in! Waiting for connection...')
 driver.get('chrome-extension://'+extensionId+'/index.html')
-sleep = 0
-while True:
-    try:
-        driver.find_element('xpath', '//*[contains(text(), "Open dashboard")]')
-        break
-    except:
-        time.sleep(1)
-        print('Loading connection...')
-        sleep += 1
-        if sleep > 30:
-            print('Could not load connection! Exiting...')
-            generate_error_report(driver)
-            driver.quit()
-            exit()
+
+try:
+    # Wait for the "Dashboard" heading to appear
+    dashboard_heading = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[contains(text(), "dashboard")]'))
+    )
+    print("Loading connection...")
+except TimeoutException:
+    print("Could not load connection! Exiting.")
+    driver.quit()
+    exit()
 
 print('Connected! Starting API...')
-#flask api
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -207,7 +222,6 @@ def get():
         generate_error_report(driver)
 
     return {'connected': connected, 'network_quality': network_quality, 'epoch_earnings': epoch_earnings}
-
 
 app.run(host='0.0.0.0',port=80, debug=False)
 driver.quit()
